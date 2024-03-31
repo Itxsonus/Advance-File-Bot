68efdde8825c86c249263c269b034a2624a5c8d3
Itxsonus <165536511+Itxsonus@users.noreply.github.com>
2024-03-31 11:45:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
-cd boted;
+git clone https://github.com/Advance_FileBot/Advance-File-Share-Bot;
+cd /Advance-File-Share-Bot;
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
 python3 bot.py;
