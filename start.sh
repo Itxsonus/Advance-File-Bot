60f4a6cd1730f9e9466aca203ce2e4966a90d963
Itxsonus <165536511+Itxsonus@users.noreply.github.com>
2024-03-31 12:11:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-rm -rf boted;
-git clone https://github.com/Advance_FileBot/Advance-File-Share-Bot;
-cd /Advance-File-Share-Bot;
-pip3 install -U -r requirements.txt;
-echo "Bot Started....";
-python3 bot.py;
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Advance_FileBot/Advance-File-Share-Bot /Advance-File-Share-Bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+fi
+cd /Advance-File-Share-Bot
+pip3 install -U -r requirements.txt
+echo "Bot Started...."
+python3 bot.py
